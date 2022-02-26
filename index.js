5294f566dd66ba33a875f77a8711f2baa3d42834
Azumabashi <hiro_kataoka@hkataoka.local>
2022-02-26 15:20:04 +0900

    update to 2022

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const http = require('http');
 const router = require('./lib/router');
 
 const server = http.createServer((req, res) => {
-  router.route(req, res);
-}).on('error', (e) => {
-  console.error('Server Error', e);
-}).on('clientError', (e) => {
-  console.error('Client Error', e);
-});
+    router.route(req, res);
+  })
+  .on('error', e => {
+    console.error('Server Error', e);
+  })
+  .on('clientError', e => {
+    console.error('Client Error', e);
+  });
 
 const port = 8000;
 server.listen(port, () => {
-  console.info('Listening on ' + port);
+  console.info(`Listening on ${port}`);
 });
